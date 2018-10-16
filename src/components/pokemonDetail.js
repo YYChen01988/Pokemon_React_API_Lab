9bb662fbe0c1d60c46878c307f20392deb6b761b
yingying <chen.yingying.01988@gmail.com>
2018-10-16 15:21:07 +0100

    try to get abilities for selected pokemon

--- src/components/pokemonDetail.js
+++ src/components/pokemonDetail.js
@@ -1,12 +1,27 @@
 import React from 'react';
 
 const PokemonDetail = (props) => {
-  if(!props.pokemon) return <p>Loading...</p>;
+  const getDetails = (url) => {
+    const abilities = [];
+    fetch(url)
+    .then((res) => res.json())
+    // .then(res => {
+    //   this.setState({abilities: abilities});
+    // })
+    // const abilities.map(ability){
+    //   return abilities.push(ability)
+    // }
 
+  }
+
+  if(!props.pokemon) return <p>Loading...</p>;
   return (
+    <div>
     <h3>
       {props.pokemon.name}
     </h3>
+      {getDetails(props.pokemon.url)}
+    </div>
   )
 }
 
